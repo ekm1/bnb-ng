8b5aa16fff9c1d7107c191ed1d9dafcca715b04e
Migel Hoxha <migelhoxha@programmer.net>
2019-01-10 15:15:27 +0100

    Added search, create rental

--- src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
+++ src/app/rental/rental-detail/rental-detail-booking/rental-detail-booking.component.ts
@@ -13,6 +13,7 @@ import { BookingService } from "../../../booking/shared/booking.service";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { ToastsManager } from "ng2-toastr/ng2-toastr";
 import { DaterangePickerComponent } from "ng2-daterangepicker";
+import { AuthService } from "../../../auth/shared/auth.service";
 import * as moment from "moment";
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -45,7 +46,8 @@ export class RentalDetailBookingComponent implements OnInit {
     private modalService: NgbModal,
     private bookingService: BookingService,
     private toastr: ToastsManager,
-    private vcr: ViewContainerRef
+    private vcr: ViewContainerRef,
+    public auth: AuthService
   ) {
     this.toastr.setRootViewContainerRef(vcr);
   }
